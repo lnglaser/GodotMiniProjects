e05a12faca95f8d90f56e4fb682a0034a48be70a
Leon Glaser <leon.glaser@gmail.com>
2023-10-05 23:10:26 -0400

    intro to Vector.ONE and queue_free()

--- BalloonPopper/Balloon.gd
+++ BalloonPopper/Balloon.gd
@@ -7,4 +7,8 @@ var score_to_give : int = 1
 
 func _on_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("Increase size")
+		scale += Vector3.ONE * size_increase
+		clicks_to_pop -= 1
+		
+		if clicks_to_pop == 0:
+			queue_free()
