f030fb195199d1765bec3dee005ede39e49c123c
Leon Glaser <leon.glaser@gmail.com>
2023-10-18 18:31:50 -0400

    Loops - complete - move instruction code to process function

--- Loops/Loops.gd
+++ Loops/Loops.gd
@@ -3,9 +3,10 @@ extends Node2D
 @export var spawn_count : int = 200
 var star_scene = preload("res://Loops/Star.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	for i in spawn_count:
+var num_stars : int = 0
+
+func _process(delta):
+	if num_stars < spawn_count:
 		var star = star_scene.instantiate()
 		add_child(star)
 		star.position.x = randi_range(-280, 280)
@@ -14,3 +15,17 @@ func _ready():
 		var star_size = randf_range(0.5, 1)
 		star.scale.x = star_size
 		star.scale.y = star_size
+		num_stars += 1
+		print(num_stars)
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+#	for i in spawn_count:
+#		var star = star_scene.instantiate()
+#		add_child(star)
+#		star.position.x = randi_range(-280, 280)
+#		star.position.y = randi_range(-150, 150)
+#
+#		var star_size = randf_range(0.5, 1)
+#		star.scale.x = star_size
+#		star.scale.y = star_size
